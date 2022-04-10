2affa7e5df40b2ab1547881238ecc7a079e52200
Hanan <hanan.e.natan@gmail.com>
2022-04-10 03:36:01 +0300

    part 2 done

--- day15/aoc15.go
+++ day15/aoc15.go
@@ -17,7 +17,7 @@ type Vertex struct {
 	weight   int
 	distance int
 	visited  bool
-	prev     Index
+	// prev     Index
 }
 
 type Index struct {
@@ -40,7 +40,7 @@ func (vt *VertexTable) CalculateDistance(from Index) {
 					cellDistance := vt.v[from.x+row[i]][from.y+col[i]].distance
 					if distance < cellDistance || cellDistance == 0 {
 						vt.v[from.x+row[i]][from.y+col[i]].distance = distance
-						vt.v[from.x+row[i]][from.y+col[i]].prev = from
+						// vt.v[from.x+row[i]][from.y+col[i]].prev = from
 					}
 				}
 
@@ -85,8 +85,6 @@ func (vt *VertexTable) PrintTable() {
 }
 
 func part1(data *VertexTable) {
-
-	// data.v[0][0].visited = true
 	data.v[0][0].weight = 0
 	visitIdx := Index{0, 0}
 	for {
@@ -96,14 +94,77 @@ func part1(data *VertexTable) {
 		}
 		visitIdx = data.GetSmallIdx()
 	}
-	// data.PrintTable()
+	fmt.Println("The distance from {0,0} to the last point is: ", data.v[data.row][data.col].distance)
+}
+
+func part2(data *VertexTable) {
+	data.v[0][0].weight = 0
+	visitIdx := Index{0, 0}
+	for {
+		data.CalculateDistance(visitIdx)
+		if data.AllVisited() {
+			break
+		}
+		visitIdx = data.GetSmallIdx()
+	}
 	fmt.Println("The distance from {0,0} to the last point is: ", data.v[data.row][data.col].distance)
 
 }
+func increaseWeight(w int) int {
+	w = (w + 1) % 9
+	if w == 0 {
+		return 9
+	}
+	return w
+}
 
-func parseData(day int) *VertexTable {
+func increaseLine(line []Vertex) []Vertex {
+	newLine := make([]Vertex, len(line), len(line))
+	for i := range line {
+		newLine[i].weight = increaseWeight(line[i].weight)
+	}
+	return newLine
+}
+
+func parseDataPart2(day int) *VertexTable {
 	file, _ := ioutil.ReadFile(fmt.Sprintf("day%d/input.txt", day))
 	// file, _ := ioutil.ReadFile(fmt.Sprintf("day%d/testinput.txt", day))
+	// file, _ := ioutil.ReadFile(fmt.Sprintf("day%d/test1.txt", day))
+	data := strings.Split(string(file), "\n")
+
+	vt := &VertexTable{
+		make([][]Vertex, (5*len(data))-1, (5*len(data) - 1)),
+		(5 * len(data)) - 1,
+		(5 * len(data[0])) - 1,
+	}
+	fmt.Println(vt.row, vt.col)
+
+	validLines := 0
+	for lineIdx, d := range data {
+		if len(d) > 0 {
+			tmp := make([]Vertex, len(d)*5)
+			for i, v := range d {
+				tmp[i].weight, _ = strconv.Atoi(string(v))
+				tmp[len(d)+i].weight = increaseWeight(tmp[i].weight)
+				tmp[i+len(d)*2].weight = increaseWeight(tmp[i+len(d)].weight)
+				tmp[i+len(d)*3].weight = increaseWeight(tmp[i+len(d)*2].weight)
+				tmp[i+len(d)*4].weight = increaseWeight(tmp[i+len(d)*3].weight)
+			}
+			vt.v[validLines] = tmp
+			vt.v[validLines+100] = increaseLine(tmp)
+			vt.v[validLines+200] = increaseLine(vt.v[lineIdx+100])
+			vt.v[validLines+300] = increaseLine(vt.v[lineIdx+200])
+			vt.v[validLines+400] = increaseLine(vt.v[lineIdx+300])
+			validLines += 1
+		}
+	}
+	vt.row = validLines*5 - 1
+	return vt
+}
+
+func parseData(day int) *VertexTable {
+	// file, _ := ioutil.ReadFile(fmt.Sprintf("day%d/input.txt", day))
+	file, _ := ioutil.ReadFile(fmt.Sprintf("day%d/testinput.txt", day))
 	// file, _ := ioutil.ReadFile(fmt.Sprintf("day%d/test1.txt", day))
 	data := strings.Split(string(file), "\n")
 
@@ -126,8 +187,11 @@ func parseData(day int) *VertexTable {
 	return vt
 }
 
-/* I've implemented the solution for part 1 using Dijkstra's Shortest Path Algorithem. I don't know much about algorithems but I watched this (https://www.youtube.com/watch?v=pVfj6mxhdMw) video and implemented the algorithem by myself.*/
+/* I've implemented the solution for part 1 using Dijkstra's Shortest Path Algorithem. I don't know much about algorithems but I watched this (https://www.youtube.com/watch?v=pVfj6mxhdMw) video and implemented the algorithem by myself.
+Part 2 needed some work in the parsing of the data. I've done a litteral job with no thinking on elegance but it worked and even though i was skeptical that i will gethe correct results with the same algorithem in a reasonable time.*/
 func main() {
-	data := parseData(15)
-	part1(data)
+	// data := parseData(15)
+	// part1(data)
+	data := parseDataPart2(15)
+	part2(data)
 }
